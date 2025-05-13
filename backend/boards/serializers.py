8b3fea4d9e952e9eba13c19937a2857733efceaa
FelipeLobos <felipelobos.dev@gmail.com>
2025-05-13 15:42:45 -0400

    Estructura backend basica (models, views, urls)

---
+++ backend/boards/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Board, List, Card
+
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = '__all__'
+
+
+class ListSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = List
+        fields = '__all__'
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    lists = ListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Board
+        fields = '__all__'
--- backend/boards/views.py
+++ backend/boards/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Board, List, Card
+from .serializers import BoardSerializer, ListSerializer, CardSerializer
+
 
 # Create your views here.
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
